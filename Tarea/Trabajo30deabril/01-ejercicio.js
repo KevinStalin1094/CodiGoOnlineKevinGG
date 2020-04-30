b6360051e879d2389e20463d7886fcde20358b47
KevinStalin1094 <kevinstalingonzalesgutierrez@gmail.com>
2020-04-30 17:22:51 -0500

    subiendo ejercicio de 30de abril20

---
+++ Tarea/Trabajo30deabril/01-ejercicio.js
@@ -0,0 +1,95 @@
+
+// Problemas propuestos
+
+// 4.1
+
+// Un profesor tiene un salario inicial de $1500, y recibe un incremento
+// de 10 % anual durante 6 años. ¿Cuál es su salario al cabo de 6
+// años? ¿Qué salario ha recibido en cada uno de los 6 años? Realice el
+// algoritmo y represente la solución mediante el diagrama de flujo, el
+// pseudocódigo y el diagrama N/S, utilizando el ciclo apropiado.
+
+
+let salarioInicial = 1500;
+let años = [1, 2, 3, 4, 5, 6];
+let incremento = 0.10;
+let meses = 12;
+let salarioAño1 = ((salarioInicial * meses) * incremento);
+let salarioAño2 = ((salarioInicial * ( meses * 2)) * incremento);
+let salarioAño3 = ((salarioInicial * ( meses * 3)) * incremento);
+let salarioAño4 = ((salarioInicial * ( meses * 4)) * incremento);
+let salarioAño5 = ((salarioInicial * (meses * 5)) * incremento);
+let salarioAño6 = ((salarioInicial * ( meses * 6)) * incremento);
+
+console.log(salarioAño1);
+console.log(salarioAño2);
+console.log(salarioAño3);
+console.log(salarioAño4);
+console.log(salarioAño5);
+console.log(salarioAño6);
+
+
+
+
+console.log("//////////////////////////////////////");
+
+
+
+// // 4.3
+// // Se requiere un algoritmo para determinar, de N cantidades, cuántas
+// // son cero, cuántas son menores a cero, y cuántas son mayores a cero.
+// // Realice el diagrama de flujo, el pseudocódigo y el diagrama N/S para
+// // representarlo, utilizando el ciclo apropiado.
+
+let n = 0;
+
+let c = n (c < 0) || (c > 0) ;
+
+
+
+
+
+
+
+
+// // 4.9
+// // Un cliente de un banco deposita equis cantidad de pesos cada mes
+// // en una cuenta de ahorros. La cuenta percibe un interés fijo durante
+// // un año de 10 % anual. Realice un algoritmo para determinar el total
+// // de la inversión final de cada año en los próximos N años. Represente
+// // la solución mediante el diagrama de flujo, el pseudocódigo y
+// // diagrama N/S.
+
+let mesx = 0;
+let añosN = +prompt("agregara los años");
+let mes = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+let clienteBancco = [x];
+let interesAnual = 0.10;
+
+
+
+
+
+
+
+
+
+
+
+
+// // 4.13
+// // Un vendedor ha realizado N ventas y desea saber cuántas fueron
+// // por 10,000 o menos, cuántas fueron por más de 10,000 pero por
+// // menos de 20,000, y cuánto fue el monto de las ventas de cada una y
+// // el monto global. Realice un algoritmo para determinar los totales.
+// // Represente la solución mediante diagrama de flujo, pseudocódigo y
+// // diagrama N/S.
+
+// let ingresarVentas = +prompt("ingresar las ventas");
+// let m = 0;
+// let vendedor = [m];
+// let ventas = (m < 10000) || (m < 2000);
+
+
+// let ventames = 0 ;
+// let ventasGlobal = 0 ;
