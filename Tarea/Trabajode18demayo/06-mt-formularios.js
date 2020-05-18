7648b2b602dc5f4f45c0a0224a4137566a86f4f5
KevinStalin1094 <kevinstalingonzalesgutierrez@gmail.com>
2020-05-18 12:43:22 -0500

    realizand Libreria MATERIAIZE

---
+++ Tarea/Trabajode18demayo/06-mt-formularios.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    M.updateTextFields();
+  });
+let ususario
+
+  $('#textarea1').val('New Text');
+  M.textareaAutoResize($('#textarea1'));
+  
+let ususario1
+  $(document).ready(function() {
+    $('input#input_text, textarea#textarea2').characterCounter();
+  });
